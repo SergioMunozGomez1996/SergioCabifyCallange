f0ccf65d48029dc72fe8270f6cd339ddb9f0e028
Sergio <sergio.munoz@ecommfarm.com>
2023-12-19 14:36:13 +0100

    Update som libraries Added launch icon

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,19 +48,17 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.5")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.5")
 
     //Coroutines
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
 
 
     //Navigation
-    val navVersion = "2.7.5"
+    val navVersion = "2.7.6"
     implementation("androidx.navigation:navigation-fragment-ktx:$navVersion")
     implementation("androidx.navigation:navigation-ui-ktx:$navVersion")
 
